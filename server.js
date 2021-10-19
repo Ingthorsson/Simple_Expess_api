550ff069f7aee784431d7ada91c44b0b10da4926
ingthorsson <ingthorsson@gmail.com>
2021-10-19 00:05:57 +0000

    Change api output

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.json({ msg: 'Hello world'}));
+app.get('/', (req, res) => res.json({ msg: 'Very basic api'}));
 
 app.listen(8080, () => console.log('Server running'));
